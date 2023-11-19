77b1f4aba29f3626c3e56d5be1aa21406e68f6b6
jeiji <jeiji@gmail.com>
2023-11-19 18:27:20 +0400

    modified

--- Moodle/MainWindow.xaml.cs
+++ Moodle/MainWindow.xaml.cs
@@ -19,45 +19,72 @@ namespace Moodle
 	
 	public partial class MainWindow : Window
 	{
-		public void register(object sender,EventArgs e)
-		{
-			string username = userbox.Text.ToString();
-			string password = passbox.Password.ToString();
-
-			string connectionString = "Data Source=DESKTOP-IBERPBJ;Initial Catalog=TEST;Integrated Security=True";
-
-			string move = "INSERT INTO LoginTable (USERNAME,USERPASSWORD) VALUES (@USERN,@PASS)";
-
-			using(SqlConnection conn = new SqlConnection(connectionString))
-			{
-				conn.Open();
-
-				using (SqlCommand addStudent = new SqlCommand(move, conn))
-				{
-					addStudent.Parameters.AddWithValue("@USERN", username);
-					addStudent.Parameters.AddWithValue("@PASS", password);
-
-					addStudent.ExecuteNonQuery();
-				}
-
-				conn.Close();
-				
-			}
-			userbox.Clear();
-			passbox.Clear();
-		}
-
-
-
 		
+
+
 
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
 
+		public void checkData(object sender,EventArgs e)
+		{
+			string username = userbox.Text.ToString();
+			string password = passbox.Password.ToString();
+			if (checkboxes()) {
+				
+			
 
+			string connectionString = "Data Source=DESKTOP-IBERPBJ;Initial Catalog=TEST;Integrated Security=True";
+
+			string move = "INSERT INTO LoginTable (USERNAME,USERPASSWORD) VALUES (@name,@pass)";
+
+			using(SqlConnection conn = new SqlConnection(connectionString)) {
+
+				conn.Open();
+
+				using (SqlCommand addStudent = new SqlCommand(move, conn))
+				{
+					addStudent.Parameters.AddWithValue("@name", username);
+					addStudent.Parameters.AddWithValue("@pass", password);
+
+					int rowsAffected = addStudent.ExecuteNonQuery();
+
+						if (rowsAffected > 0)
+						{
+							MessageBox.Show("Student added Succesfully");
+						}
+						else
+						{
+							MessageBox.Show("Error");
+						}
+					}
+				conn.Close();
+			}
+			}
+			else
+			{
+				MessageBox.Show("Fill all fields");
+			}
+			userbox.Clear();
+			passbox.Clear();
+		}
 		
+		private bool checkboxes()
+		{
+			string username = userbox.Text.ToString();
+			string password = passbox.Password.ToString();
+
+			if(username == "" || password == "")
+			{
+				userbox.Clear();
+				passbox.Clear();
+				return false;
+			}
+			return true;
+			
+		}
 	}
 	
 }
